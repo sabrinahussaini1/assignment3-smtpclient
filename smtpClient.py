8550def2319c2c9954569f28d37154e5a9cb4f5b
sabrinahussaini1 <sh8448@nyu.edu>
2025-02-22 17:20:00 -0600

    Update smtpClient.py

--- smtpClient.py
+++ smtpClient.py
@@ -9,45 +9,66 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
 
-    # Fill in startd hdjudhjs
+    # Fill in started
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((mailserver,port))
     # Fill in end
 
     recv = clientSocket.recv(1024).decode()
     #print(recv) #You can use these print statement to validate return codes from the server.
     #if recv[:3] != '220':
-    #    print('220 reply not received from server.')
+     #   print('220 reply not received from server.')
 
     # Send HELO command and print server response.
     heloCommand = 'HELO Alice\r\n'
     clientSocket.send(heloCommand.encode())
     recv1 = clientSocket.recv(1024).decode()
-    #print(recv1) 
+    #print(recv1)
     #if recv1[:3] != '250':
-    #    print('250 reply not received from server.')
+     #   print('250 reply not received from server.')
 
     # Send MAIL FROM command and handle server response.
     # Fill in start
+    mailFromCommand = 'MAIL FROM: <person@gmail.com>\r\n'
+    clientSocket.send(mailFromCommand.encode())
+
     # Fill in end
 
     # Send RCPT TO command and handle server response.
     # Fill in start
+    rcptToCommand = 'RCPT TO: <person@mail.com>\r\n'
+    clientSocket.send(rcptToCommand.encode())
+   
     # Fill in end
 
     # Send DATA command and handle server response.
     # Fill in start
+    dataCommand = 'DATA\r\n'
+    clientSocket.send(dataCommand.encode())
+    
     # Fill in end
 
     # Send message data.
     # Fill in start
+    clientSocket.send(msg.encode())
     # Fill in end
 
     # Message ends with a single period, send message end and handle server response.
     # Fill in start
+    clientSocket.send(endmsg.encode())
+    recv_end = clientSocket.recv(1024).decode()
+    #print(recv_end)
+    #if recv_end[:3] != '221':
+        #print('221 reply not received from server') 
     # Fill in end
 
     # Send QUIT command and handle server response.
     # Fill in start
+    quitCommand = 'QUIT\r\n'
+    clientSocket.send(quitCommand.encode())
     # Fill in end
+
+    clientSocket.close()
 
 
 if __name__ == '__main__':
